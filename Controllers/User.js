6191888b746b629ab1c4edd5a680cd63c3cb004e
Jose Guerrero <wichoguerrero@yahoo.com>
2019-02-20 23:00:15 -0600

    adding authenticate

--- Controllers/User.js
+++ Controllers/User.js
@@ -1,4 +1,5 @@
 const UserModel = require('../Models/User')
+const jwt = require('jsonwebtoken')
 
 function findAll (req, res, next) {
   UserModel.find({}, (err, users) => {
@@ -13,13 +14,41 @@ function createUser (req, res, next) {
   let newUser = new UserModel(req.body)
   newUser.save((err, user) => {
     if (err) {
-      throw err
+      res.status(404).json({ status: 'error', message: 'manda los datos requeridos' })
     }
     res.send(user)
   })
 }
 
+function authenticate (req, res, next) {
+  UserModel.find({ Correo: req.headers.correo }, (err, UserInfo) => {
+    if (err) {
+      res.status(404).json({ status: 'error', message: err }, { data: null })
+    } else {
+      if (req.headers.password === UserInfo[0].Password) {
+        const token = jwt.sign({ id: UserInfo._id }, 'publicKey', { expiresIn: '8hr' })
+        res.status(200).json({ status: 'success', message: 'usuario encontrado', data: { user: UserInfo[0], token: token } })
+      } else {
+        res.status(404).json({ status: 'error', message: err }, { data: null })
+      }
+    }
+  })
+}
+
+function verify (req, res, next) {
+  jwt.verify(req.headers['x-token'],
+    'publicKey', (err, decoded) => {
+      if (err) {
+        res.status(400).json({ status: 'error', message: err.message, data: null })
+      } else {
+        req.body.userId = decoded.id
+        next()
+      }
+    })
+}
 module.exports = {
   findAll,
-  createUser
+  createUser,
+  authenticate,
+  verify
 }
--- app.js
+++ app.js
@@ -7,13 +7,20 @@ const mongoose = require('mongoose')
 
 const User = require('./routes/User')
 
-mongoose.connect('mongodb://localhost:27017/appArcsa', { useNewUrlParser: true },() => console.log('Escuchando DB'))
+mongoose.connect('mongodb://localhost:27017/appArcsa', { useNewUrlParser: true }, () => console.log('Escuchando DB'))
 
 app.use(Bodyparser.json())
 app.use(Bodyparser.urlencoded({ extended: true }))
 
-// rutas
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Content-Type', 'application/x-www-form-urlencoded')
+  res.header('Access-Control-Request-Method: POST')
+  next()
+})
 
+
+// rutas
 app.use('/user', User)
 
 app.listen(PORT, () => console.log(`Conectado a Localhost:${PORT}`))
